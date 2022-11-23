8a733ac6f82863c5d5a019fd1bfcf31abbcf8421
Bharat Bhatia <bharatbhatia_1991@yahoo.in>
2022-11-23 23:06:40 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-graphql-test'`, () => {
+  it(`should have as title 'angular-graphql'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-graphql-test');
+    expect(app.title).toEqual('angular-graphql');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-graphql-test app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-graphql app is running!');
   });
 });
